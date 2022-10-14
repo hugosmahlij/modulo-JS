5b9e21776d592f525372d3bfa31a01b7081aa600
hugosmahlij <smahlij@gmail.com>
2022-10-14 19:08:39 -0300

    change method js and fix files

--- TASK2/assets/js/main.js
+++ TASK2/assets/js/main.js
@@ -1,12 +1,15 @@
 const container = document.getElementById("main_container");
+const date = data.currentDate
+const title = document.getElementById("title")
+const card = data.events.map(Card => Card)
 
-for (const event of data.events) {
-    getCard(event);
-}
+const homeCard = card.filter(()=>title.textContent.includes("Home")).forEach(getCard)
+const upcomingCard = card.filter(()=>title.textContent.includes("Upcoming")).filter((card) => date > card.date).forEach(getCard)
+const pastCard = card.filter(()=>title.textContent.includes("Past")).filter((card) => date < card.date).forEach(getCard)
 
 function getCard(event){
     container.innerHTML += `
-    <div class="card mt-5 " style="width: 18rem">
+    <div class="card mt-5 height" style="width: 18rem">
           <img src="${event.image}" class="card-img-top" alt="${event.name}" />
           <div
             class="card-body d-flex flex-column align-items-center justify-content-evenly shadow"
@@ -17,7 +20,7 @@ function getCard(event){
             </p>
             <div class="w-100 d-flex align-items-center justify-content-evenly">
               <p>Price $${event.price}</p>
-              <a href="#" class="d-flex btn btn-danger">Details</a>
+              <a href="../pages/details.html" class="d-flex btn btn-danger">Details</a>
             </div>
           </div>
         </div>
