1bc17029dbda446255596432f79bb17472092b01
hugosmahlij <smahlij@gmail.com>
2022-10-17 14:55:59 -0300

    change some files and fixed others

--- TASK2/assets/js/main.js
+++ TASK2/assets/js/main.js
@@ -1,15 +1,32 @@
 const container = document.getElementById("main_container");
 const date = data.currentDate
 const title = document.getElementById("title")
+const inputSearch = document.getElementById("search")
+
 const card = data.events.map(Card => Card)
 
-const homeCard = card.filter(()=>title.textContent.includes("Home")).forEach(getCard)
-const upcomingCard = card.filter(()=>title.textContent.includes("Upcoming")).filter((card) => date > card.date).forEach(getCard)
-const pastCard = card.filter(()=>title.textContent.includes("Past")).filter((card) => date < card.date).forEach(getCard)
+const homeCard = card.filter(() =>title.textContent.includes("Home"))
+const upcomingCard = card.filter(() =>title.textContent.includes("Upcoming")).filter((card) =>date > card.date)
+const pastCard = card.filter(() =>title.textContent.includes("Past")).filter((card) =>date < card.date)
+
+homeCard.forEach(getCard)
+upcomingCard.forEach(getCard)
+pastCard.forEach(getCard)
+
+inputSearch.addEventListener(`input`,searchBox)
+
+function searchBox(search){
+  let homeSearch = homeCard.filter(event => event.name.toLowerCase().includes(search.target.value.toLowerCase()))
+  let upcomingSearch = upcomingCard.filter(event => event.name.toLowerCase().includes(search.target.value.toLowerCase()))
+  let pastSearch = pastCard.filter(event => event.name.toLowerCase().includes(search.target.value.toLowerCase()))
+  container.innerHTML = ``
+  let generalSearch = [...homeSearch,...upcomingSearch,...pastSearch]
+  generalSearch.forEach(getCard);
+}
 
 function getCard(event){
     container.innerHTML += `
-    <div class="card mt-5 height" style="width: 18rem">
+    <div class="card mt-4 m-1" style="width: 18rem">
           <img src="${event.image}" class="card-img-top" alt="${event.name}" />
           <div
             class="card-body d-flex flex-column align-items-center justify-content-evenly shadow"
